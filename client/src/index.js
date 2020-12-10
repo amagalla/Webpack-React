6e4f7c5e2c35fb0bbe367b93e2a62367d6458d92
Anthony Magallanes <anthonymagallanes24@gmail.com>
2020-12-09 16:10:09 -0800

    Changed css to scss

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App'
 
-import './styles/styles.css'
+import './styles/styles.scss'
 
 render(<App />, document.getElementById("root"))
