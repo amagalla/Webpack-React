581cacf52b7def3f0bd43804b2871707fdb20d18
amagalla <anthonymagallanes24@gmail.com>
2021-03-23 14:51:17 -0700

    Updated webpack and package.json to include sass loader for sass functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: ['./client/index.js'],
+  entry: ["./client/index.js"],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.jsx?/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.(scss|css)$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(gif|png|jpe?g)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/',
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
             },
           },
         ],
       },
       {
         test: /\.html$/,
-        use: ['html-loader'],
+        use: ["html-loader"],
       },
     ],
   },
@@ -44,12 +44,12 @@ module.exports = {
     port: 8080,
     open: true,
     hot: true,
-    publicPath: '/dist/',
+    publicPath: "/dist/",
     historyApiFallback: true,
     proxy: [
       {
-        context: ['/api/**'],
-        target: 'http://localhost:3000',
+        context: ["/api/**"],
+        target: "http://localhost:3000",
       },
     ],
   },
